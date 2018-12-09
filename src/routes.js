b1ec2e867b2685e4e3a09b4f6adea57099592c8f
Deniz Mersinlioğlu <mersinliogludeniz@gmail.com>
2018-12-10 01:03:48 +0300

    Stripes Implemented

--- src/routes.js
+++ src/routes.js
@@ -1,11 +1,15 @@
 import React from 'react';
 import Layout from './Hoc/Layout';
 import { Switch, Route } from 'react-router-dom';
+
+import Home from './Components/home';
 const Routes = props => {
   return (
     <div>
       <Layout>
-        <Switch>asd</Switch>
+        <Switch>
+          <Route exact component={Home} path='/' />
+        </Switch>
       </Layout>
     </div>
   );
